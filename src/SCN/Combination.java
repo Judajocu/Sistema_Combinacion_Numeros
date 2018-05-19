e2f6cc32b8ffef571a89e0e6fc8b1d234adbcf9a
judajocu <juandaniejoa@gmail.com>
2018-05-19 09:38:00 -0400

    Hecho correctamente, backend almost done(truly this time)

--- src/SCN/Combination.java
+++ src/SCN/Combination.java
@@ -1,44 +1,49 @@
 package SCN;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Combination
 {
-    static int[] Combine(int Nun2Combine, int[] Combined, int ArrSize)
-    {
-        int[] binary = new int[(int) Math.pow(2, ArrSize)];
-        for (int i = 0; i < Math.pow(2, ArrSize); i++) {
-            int b = 1;
-            binary[i] = 0;
-            int num = i, count = 0;
-            while (num > 0) {
-                if (num % 2 == 1)
-                    count++;
-                binary[i] += (num % 2) * b;
-                num /= 2;
-                b = b * 10;
-            }
-            if (count == Nun2Combine) {
-                System.out.print("{ ");
-                for (int j = 0; j < ArrSize; j++) {
-                    if (binary[i] % 10 == 1)
-                        System.out.print(Combined[j] + " ");
-                    binary[i] /= 10;
-                }
-                System.out.println("}");
-            }
+    private static void getSubsets(List<Integer> superSet, int k, int idx, Set<Integer> current, List<Set<Integer>> solution) {
+        //successful stop clause
+        if (current.size() == k) {
+            solution.add(new HashSet<>(current));
+            return;
         }
-        return Combined;
+        //unseccessful stop clause
+        if (idx == superSet.size()) return;
+        Integer x = superSet.get(idx);
+        current.add(x);
+        //"guess" x is in the subset
+        getSubsets(superSet, k, idx+1, current, solution);
+        current.remove(x);
+        //"guess" x is not in the subset
+        getSubsets(superSet, k, idx+1, current, solution);
+    }
+
+    public static List<Set<Integer>> getSubsets(List<Integer> superSet, int k) {
+        List<Set<Integer>> res = new ArrayList<>();
+        getSubsets(superSet, k, 0, new HashSet<Integer>(), res);
+        return res;
     }
 
 
-    static int[] StringArr2Int(String Numbers)
+    static List<Integer> StringArr2Int(String Numbers)
     {
         String[] integerStrings = Numbers.split(" ");
-        int[] integers = new int[integerStrings.length];
-        for (int i = 0; i < integers.length; i++)
+        List<Integer> Converted = new ArrayList<>();
+        for (int i = 0; i < integerStrings.length; i++)
         {
-            integers[i] = Integer.parseInt(integerStrings[i]);
+            int num = Integer.parseInt(integerStrings[i]);
+            Converted.add(num);
         }
-        return integers;
+        return Converted;
     }
 
     static int String2Int(String Number)
@@ -47,13 +52,28 @@ public class Combination
         return num;
     }
 
+    static void Result2txt(int[] result, String FileName) throws IOException {
+        BufferedWriter outputWriter = null;
+        outputWriter = new BufferedWriter(new FileWriter(FileName));
+        for (int i = 0; i < result.length; i++) {
+            // Maybe:
+            outputWriter.write(result[i]+"");
+            // Or:
+            outputWriter.write(Integer.toString(result[i]));
+            outputWriter.newLine();
+        }
+        outputWriter.flush();
+        outputWriter.close();
+    }
+
         /*Driver function to check for above function*/
         public static void main (String[] args) {
             String arr = "1 2 3 4 5";
+            List<Integer> dat = new ArrayList<>();
             String r = "3";
             int size = String2Int(r);
-            int dat[] = StringArr2Int(arr);
-            int result[] = Combine(size,  dat , dat.length);
+            dat = StringArr2Int(arr);
+            System.out.println(getSubsets(dat,size));
         }
 
 
